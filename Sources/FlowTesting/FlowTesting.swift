adf34777478c00412d0552bc298601655e2b1c4e
Beau Ayres <beau@beauayres.com.au>
2026-04-13 17:36:24 +1000

    Add FlowTesting umbrella re-exporting test infrastructure

---
+++ Sources/FlowTesting/FlowTesting.swift
@@ -0,0 +1,8 @@
+// FlowKit testing umbrella library target.
+//
+// Re-exports FlowSharedModels (for FlowTestError), FlowTestClock (for TestClock),
+// and FlowTestingCore (for FlowTester, ThrowingFlowTester, TestScope).
+
+@_exported import FlowSharedModels
+@_exported import FlowTestClock
+@_exported import FlowTestingCore
